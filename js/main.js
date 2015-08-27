bd9036f4fdafd760dee21d898ca646e78458e1c5
User <noemail@nomail.no>
2015-08-27 16:36:11 -0700

    put all logic to the ViewModel

--- js/main.js
+++ js/main.js
@@ -1,81 +1,139 @@
  var places = [{
-     name: "Clocktower Coffee Roasting Co.",
-     address: "205 E Middlefield Rd #1A Mountain View, CA 94043",
-     lat: 37.3970472,
-     lng: -122.0617372,
-     iconType: "coffee"
+ 	name: "Clocktower Coffee Roasting Co.",
+ 	address: "205 E Middlefield Rd #1A Mountain View, CA 94043",
+ 	lat: 37.3970472,
+ 	lng: -122.0617372,
+ 	iconType: "coffee"
  }, {
-     name: "Red Rock Coffee",
-     address: "201 Castro St Mountain View, CA 94041",
-     lat: 37.3936357,
-     lng: -122.0788748,
-     iconType: "coffee"
+ 	name: "Sufi Coffee Shop",
+ 	address: "815 W El Camino Real Mountain View, CA 94040",
+ 	lat: 37.385884,
+ 	lng: -122.0847306,
+ 	iconType: "coffee"
  }, {
-     name: "Sufi Coffee Shop",
-     address: "815 W El Camino Real Mountain View, CA 94040",
-     lat: 37.385884,
-     lng: -122.0847306,
-     iconType: "coffee"
+ 	name: "Oh My Sushi",
+ 	address: "2595 California St Mountain View, CA 94040",
+ 	lat: 37.4053954,
+ 	lng: -122.1107026,
+ 	iconType: "sushi"
  }, {
-     name: "Sono Sushi",
-     address: "357 Castro St #3A Mountain View, CA 94041",
-     lat: 37.3917347,
-     lng: -122.0799789,
-     iconType: "sushi"
+ 	name: "Hon Sushi",
+ 	address: "1477 Plymouth St Mountain View, CA 94043",
+ 	lat: 37.4163068,
+ 	lng: -122.0795238,
+ 	iconType: "sushi"
  }, {
-     name: "Oh My Sushi",
-     address: "2595 California St Mountain View, CA 94040",
-     lat: 37.4053954,
-     lng: -122.1107026,
-     iconType: "sushi"
- }, {
-     name: "Hon Sushi",
-     address: "1477 Plymouth St Mountain View, CA 94043",
-     lat: 37.4163068,
-     lng: -122.0795238,
-     iconType: "sushi"
- }, {
-     name: "Satsuma Sushi",
-     address: "705 E El Camino Real Mountain View, CA 94040",
-     lat: 37.3755308,
-     lng: -122.0638239,
-     iconType: "sushi"
+ 	name: "Satsuma Sushi",
+ 	address: "705 E El Camino Real Mountain View, CA 94040",
+ 	lat: 37.3755308,
+ 	lng: -122.0638239,
+ 	iconType: "sushi"
  }];
 
  var icons = {
-     sushi: {
-         icon: "images/sushi.png"
-     },
-     coffee: {
-         icon: "images/coffee.png"
-     }
+ 	sushi: {
+ 		icon: "images/sushi.png"
+ 	},
+ 	coffee: {
+ 		icon: "images/coffee.png"
+ 	}
  };
 
- var Marker = function(place, map) {
- 	
-     var self = this;
-     this.name = place.name;
-     this.address = place.address;
-     this.lat = place.lat;
-     this.lng = place.lng;
-     this.location = new google.maps.LatLng(place.lat, place.lng);
-     this.mapMarker = new google.maps.Marker({
-         position: this.location,
-         map: map,
-         icon: icons[place.iconType].icon
-     });
-     var infoLocation = place.lat + "," + place.lng;
-     var infowindow;
-     var imageUrl = "https://maps.googleapis.com/maps/api/streetview?size=100x100&location=" + infoLocation;
+ var Marker = function(place) {
+ 	var self = this;
+ 	this.name = place.name;
+ 	this.address = place.address;
+ 	this.lat = place.lat;
+ 	this.lng = place.lng;
+ 	this.iconType = place.iconType;
+ 	this.infoWindow = null;
+ 	this.mapMarker = null;
+ 	this.activate = null;
+ 	this.setInfoWindow = function(infowindow) {
+ 		this.infoWindow = infowindow;
+ 	}
+ 	this.setMapMarker = function(mapMarker) {
+ 		this.mapMarker = mapMarker;
+ 	}
+ 	this.setActivate = function(activate) {
+ 		this.activate = activate;
+ 	}
+ };
 
-     // create info window for each marker with information from foursquare api
+ var ViewModel = function() {
+ 	var self = this;
+ 	var map;
+ 	this.markers = ko.observableArray([]);
+ 	this.filteredMarkers = ko.observableArray([]);
+ 	this.query = ko.observable("");
+ 	this.initializeMap = function() {
+ 		places.forEach(function(item) {
+ 			self.markers.push(new Marker(item));
+ 		});
+         // create copy of existing markers to use it for filtering
+         for (var i = 0; i < self.markers().length; i++) {
+         	self.filteredMarkers.push(self.markers()[i]);
+         }
+     // create the map
+     var mapCanvas = document.getElementById('map-canvas');
+     var mapOptions = {
+     	center: new google.maps.LatLng(37.4006792, -122.068216),
+     	zoom: 14,
+     	mapTypeId: google.maps.MapTypeId.ROADMAP,
+     	panControl: false,
+     	zoomControl: true,
+     	zoomControlOptions: {
+     		position: google.maps.ControlPosition.RIGHT_TOP
+     	},
+     	scaleControl: true
+     };
+     map = new google.maps.Map(mapCanvas, mapOptions);
+     var sw = new google.maps.LatLng(37.3989458, -122.1107519);
+         var ne = new google.maps.LatLng(37.4024914, -122.0527303);
+         var mapBounds = new google.maps.LatLngBounds(sw, ne);
+         map.fitBounds(mapBounds);
+         window.addEventListener('resize', function(e) {
+             map.fitBounds(mapBounds);
+         });
+ };
+
+ this.initializeMarkers = function() {
+ 	for (var i = 0; i < self.markers().length; i++) {
+ 		var marker = self.markers()[i];
+ 		var mapMarker = new google.maps.Marker({
+   		 	position: new google.maps.LatLng(marker.lat, marker.lng),
+    		map: map,
+    		icon: icons[marker.iconType].icon
+ 		 });
+ 		marker.setMapMarker(mapMarker);
+ 		marker.setActivate(activateMarker);
+
+ 		mapMarker.addListener('click', (function(marker) {
+ 			return function() { 
+    			activateMarker(marker);
+  			}
+ 		})(marker));
+ 	}
+ };
+ function activateMarker(marker) {
+ 	marker.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
+         		setTimeout(function() {
+             		marker.mapMarker.setAnimation(null);
+         		}, 1400);
+
+ 	if (marker.infoWindow != null) {
+ 		marker.infoWindow.open(map, marker.mapMarker);
+ 	} else {
+ 	 var markerLocation = marker.lat + "," + marker.lng;
+     var imageUrl = "https://maps.googleapis.com/maps/api/streetview?size=100x100&location=" + markerLocation;
      var CLIENT_ID = "O4L3T0YIVRWVHCWIQ1Y1UHUXUDRBQ3S4AXKT4ZIQTSIKQMNN";
      var CLIENT_SECRET = "5KVONUBGB0YJZJFMTAIXKGCMRB15KEVAGRY2YWMCCZF03EXC";
-     var compactVenueUrl = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815&query=" + self.name + "&intent=match&ll=" + infoLocation;
-     $.getJSON(compactVenueUrl, function(data) {
+     var compactVenueUrl = "https://api.foursquare.com/v2/venues/search?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815&query=" + marker.name + "&intent=match&ll=" + markerLocation;
+
+ 	$.getJSON(compactVenueUrl, function(data) {
              var venueDetailUrl = "https://api.foursquare.com/v2/venues/" + data.response.venues[0].id + "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20130815";
              $.getJSON(venueDetailUrl, function(data) {
-             	createInfoWindow(imageUrl, data.response.venue);
+             	createInfoWindow(marker, imageUrl, data.response.venue);
              }).error(function() {
              	createInfoWindowWithErrorMessage();
              });
@@ -84,86 +142,73 @@
          	createInfoWindowWithErrorMessage();
          });
 
-     function createInfoWindow(image, venue) {
-     	var container = document.createElement('div');
+ 		
+ 	}
+ }
+
+ function createInfoWindow(marker, image, venue) {
+ 	var container = document.createElement('div');
          container.setAttribute("class", "info-window-container");
-         $('<img src="' + image + '" class="info-image">').appendTo(container);
+         $('<img>', {
+         	class: "info-image",
+         	src: '' + image
+         }).appendTo(container);
 
-         var content = $('<div class="content"></div>');
-         content.appendTo(container);
+         var content = $('<div>', {
+         	class: "content"
+         }).appendTo(container);
 
-         $('<h1 class="venue-name">' + venue.name + '<h1>').appendTo(content);
+         $('<h1>', {
+         	class: "venue-name",
+         	text: '' + venue.name
+         }).appendTo(content);
 
-         var venueAddress = venue.location.address + ", " + venue.location.city + ", <br> " + venue.location.postalCode + ", " + venue.location.state;
-         $('<h2 class="venue-address">' + venueAddress + '</h2><br>').appendTo(content);
+         var venueAddress = venue.location.address + ", " + venue.location.city + ", " + venue.location.postalCode + ", " + venue.location.state;
+         $('<h2>', {
+         	class: "venue-address",
+         	text: '' + venueAddress
+         }).appendTo(content);
 
-         $('<span class="venue-rating">Rating: ' + venue.rating + '/10</span>').appendTo(content);
-         $('<a class="details-link" href="' + venue.canonicalUrl + '">View details</a><br>').appendTo(content);
+         var text = "Rating: " + venue.rating + "/10";
+         $('<span>', {
+         	class: "venue-rating",
+         	text: '' + text
+         }).appendTo(content);
+
+         $('<a>', {
+         	class: "details-link",
+         	href: '' + venue.canonicalUrl,
+         	text: "View details"
+         }).appendTo(content);
+
          if (venue.hasOwnProperty("phrases")) {
-             $('<hr><span class="review">' + venue.phrases[0].sample.text + '</span>').appendTo(content);
+             $('<hr>').appendTo(content);
+             $('<span>', {
+         	class: "review",
+         	text: '' + venue.phrases[0].sample.text
+         }).appendTo(content);
+
          }
-         infowindow = new google.maps.InfoWindow({
-                     content: container
-                 })
-
-
-
-        // if ('content' in document.createElement('template')) {
-         	// var template = document.getElementById("test");
-         	// //a = template.content.getElementById("venue-name");
-         	// var newcontainer = document.getElementById("testTemplate");
-         	// var clone = document.importNode(template.content, true);
-         	// newcontainer.appendChild(clone);
-
-
-
-         // } else {
-
-         // }
-
-
-     }
-
-     function createInfoWindowWithErrorMessage() {
-     	infowindow = new google.maps.InfoWindow({
-                     content: "<div class='" + "error-message" + "'>Failed to load venue details from foursquare server</div>"
-                 });
-     }
-
-     this.mapMarker.addListener('click', standOut);
-
-     function standOut() {
-         infowindow.open(map, self.mapMarker);
-         self.mapMarker.setAnimation(google.maps.Animation.BOUNCE);
-         setTimeout(function() {
-             self.mapMarker.setAnimation(null);
-         }, 1400);
-     }
-     this.showMarkerInfo = function() {
-         standOut();
-     };
- };
-
- var ViewModel = function() {
- 	this.temp = ko.observable("hello Template");
-
-     var self = this;
-     var map;
-     this.markers = ko.observableArray([]);
-     this.filteredMarkers = ko.observableArray([]);
-     this.query = ko.observable("");
-
-     // function displays markers on the map
-     function setMapForMarkers(markers, map) {
-         for (var i = 0; i < markers.length; i++) {
-             markers[i].mapMarker.setMap(map);
+ 	var infowindow = new google.maps.InfoWindow({
+    	content: container
+  		});
+ 	marker.setInfoWindow(infowindow);
+ 	infowindow.open(map, marker.mapMarker);
+ }
+function removeMarkersFromMap() {
+	for (var i = 0; i < self.filteredMarkers().length; i++) {
+             self.filteredMarkers()[i].mapMarker.setMap(null);
          }
-     }
-
-     // function filters markers
+}
+function setMapForMarkers() {
+	for (var i = 0; i < self.filteredMarkers().length; i++) {
+             self.filteredMarkers()[i].mapMarker.setMap(map);
+         }
+}
+ // function filters markers
      this.filter = function() {
          // remove markers from the map
-         setMapForMarkers(self.filteredMarkers(), null);
+         removeMarkersFromMap();
          // remove markers from the list view
          self.filteredMarkers.removeAll();
          // find the markers whose name or address contains query string and add them to the list view
@@ -175,44 +220,7 @@
              }
          }
          // add filtered markers on the map
-         setMapForMarkers(self.filteredMarkers(), map);
-     };
-
-     // function creates markers, create map and adds markers on the map
-     this.initializeMap = function() {
-         // create the map
-         var mapCanvas = document.getElementById('map-canvas');
-         var mapOptions = {
-             center: new google.maps.LatLng(37.4006792, -122.068216),
-             zoom: 14,
-             mapTypeId: google.maps.MapTypeId.ROADMAP,
-             panControl: false,
-             zoomControl: true,
-             zoomControlOptions: {
-                 position: google.maps.ControlPosition.RIGHT_TOP
-             },
-             scaleControl: true
-         };
-
-
-         map = new google.maps.Map(mapCanvas, mapOptions);
-         // create Markers from objects
-         places.forEach(function(item) {
-             self.markers.push(new Marker(item, map));
-         });
-         // create copy of existing markers to use it for filtering
-         for (var i = 0; i < self.markers().length; i++) {
-             self.filteredMarkers.push(self.markers()[i]);
-         }
-         var sw = new google.maps.LatLng(37.3989458, -122.1107519);
-         var ne = new google.maps.LatLng(37.4024914, -122.0527303);
-         var mapBounds = new google.maps.LatLngBounds(sw, ne);
-         map.fitBounds(mapBounds);
-         window.addEventListener('resize', function(e) {
-             console.log("resized");
-             map.fitBounds(mapBounds);
-         });
-
+         setMapForMarkers();
      };
 
      this.toggleListView = function() {
@@ -226,16 +234,12 @@
          }
 
      }
-     		var template = document.getElementById("test");
-         	//a = template.content.getElementById("venue-name");
-         	var newcontainer = document.getElementById("testTemplate");
-         	var clone = document.importNode(template.content, true);
-         	newcontainer.appendChild(clone);
 
- };
- var viewModel = new ViewModel();
- ko.applyBindings(viewModel);
- viewModel.initializeMap();
- viewModel.query.subscribe(function() {
+};
+var viewModel = new ViewModel();
+ko.applyBindings(viewModel);
+viewModel.initializeMap();
+viewModel.initializeMarkers();
+viewModel.query.subscribe(function() {
      viewModel.filter();
  });
